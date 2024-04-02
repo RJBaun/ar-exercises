052d23714dea2fa7106d7cd269a09479e8bd2aab
Rylan Baun <rylanjbaun@gmail.com>
2024-04-01 22:58:15 -0600

    Complete exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,10 +5,21 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+require 'active_record'
+
 puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
 
 @store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
 @store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 65)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,28 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, in: 40..200}
+  validates :store, presence: true
+
+  @store_name = gets.chomp
+  store = Store.create(
+  name: @store_name, 
+  annual_revenue: ,
+  mens_apparel: ,
+  womens_apparel: 
+  )
+
+  if store.errors.any?
+    store.errors.full_messages.each do |message|
+      puts message
+    end
+  end
+end
